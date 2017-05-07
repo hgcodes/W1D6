1d95bb5d04614f0820de6e2cb1cbb892b183d11a
sevenveils <hghamilton@gmail.com>
2017-05-07 22:19:26 +0000

    modified callback to display IncomingMessage

--- https_example.js
+++ https_example.js
@@ -1,6 +1,6 @@
 var https = require('https');
 
-console.log('I did it!');
+// console.log('I did it!');
 
 var options = {
   host: 'www.example.org',
@@ -8,8 +8,9 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
@@ -17,3 +18,4 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
